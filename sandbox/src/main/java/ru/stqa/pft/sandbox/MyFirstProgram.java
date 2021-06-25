22013b49bea6e228a860ae4e44427c7685cb2e7c
Andriy Petrenko <petrenkodrys@gmail.com>
2021-06-26 01:30:13 +0300

    добавил методы и классы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,20 +6,17 @@ public class MyFirstProgram {
         hello("world");
         hello("jija");
         hello("Andrey");
-        double len = 5;
-        System.out.println("Площадь квадрата со стороной "+ len +" см = " + area(len));
 
-        double a = 4;
-        double b = 5;
-        System.out.println("Площадь прямоугольника со сторонами "+ a +" и = " + b +" = " + area(a, b));
+
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной "+ s.l +" см = " + s.area());
+
+        Rectangle r = new Rectangle(4,6);
+        System.out.println("Площадь прямоугольника со сторонами "+ r.a +" и = " + r.b +" = " + r.area());
     }
     public static void hello(String world){
         System.out.println("Hello, " + world +"!");
     }
-    public static double area(double l){
-        return l*l;
-    }
-    public static double area(double a, double b){
-        return a*b;
-    }
+
+
 }
