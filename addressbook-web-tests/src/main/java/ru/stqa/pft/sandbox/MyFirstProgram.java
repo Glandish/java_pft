3ca76e55e057b75b6f315774e0db6cb6fe929c16
Andriy Petrenko <petrenkodrys@gmail.com>
2021-07-07 16:36:12 +0300

    Code Refactored

---
+++ addressbook-web-tests/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,22 @@
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+        hello("world");
+        hello("jija");
+        hello("Andrey");
+
+
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной "+ s.l +" см = " + s.area());
+
+        Rectangle r = new Rectangle(4,6);
+        System.out.println("Площадь прямоугольника со сторонами "+ r.a +" и = " + r.b +" = " + r.area());
+    }
+    public static void hello(String world){
+        System.out.println("Hello, " + world +"!");
+    }
+
+
+}
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/SecondProgram.java
@@ -0,0 +1,7 @@
+package ru.stqa.pft.sandbox;
+
+public class SecondProgram {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
